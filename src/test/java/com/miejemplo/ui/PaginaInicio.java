25507e63b3fae0399906bd4f9431867f6916fc38
Luis-Ospino <luisospinodev@gmail.com>
2026-03-16 00:48:20 -0500

    se corrige ruta del xpath y soluciona problema de ejecutar test

--- src/test/java/com/miejemplo/ui/PaginaInicio.java
+++ src/test/java/com/miejemplo/ui/PaginaInicio.java
@@ -5,5 +5,5 @@ import net.serenitybdd.screenplay.targets.Target;
 public class PaginaInicio {
 
     public static final Target CAJA_BUSQUEDA = Target.the("caja de búsqueda")
-            .locatedBy("input[id='yschsp']");
+            .locatedBy("//input[@name='p']");
 }
